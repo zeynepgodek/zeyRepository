c6baab8c9a43478c60b4db7c92151b4e1518c56e
Zeynep Godek <zey.godek@gmail.com>
2018-05-08 11:24:04 +0300

    Deneme yapılıyor...

---
+++ index.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.7.2, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.7.2, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="http://[::1]/dijitalSandik/assets/images/logo4.png" type="image/x-icon">
+  <meta name="description" content="Dijital Sandık">
+  <title>Dijital Sandık</title>
+
+  <link rel="stylesheet" href="http://[::1]/dijitalSandik//assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="http://[::1]/dijitalSandik//assets/tether/tether.min.css">
+  <link rel="stylesheet" href="http://[::1]/dijitalSandik//assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="http://[::1]/dijitalSandik//assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="http://[::1]/dijitalSandik//assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="http://[::1]/dijitalSandik//assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="http://[::1]/dijitalSandik/assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="http://[::1]/dijitalSandik/assets/theme/css/style.css">
+  <link rel="stylesheet" href="http://[::1]/dijitalSandik/assets/mobirise/css/mbr-additional.css" type="text/css">
+  <link rel="stylesheet" href="http://[::1]/dijitalSandik/assets/arkaplan.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-qQIQZqEfMx" once="menu" id="menu1-2">
+
+
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="#">
+                       <img src="http://[::1]/dijitalSandik/assets/images/ds.png" alt="Dijital Sandık" style="height: 3.8rem;">
+                   </a>
+               </span>
+               <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-5" href="#">DİJİTAL SANDIK</a></span>
+           </div>
+       </div>
+       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+            <a class="nav-link link text-white display-4" href="#">
+            </a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link link text-white display-4" href="#" aria-expanded="false"><span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span>
+            ANA SAYFA &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;</a>
+        </li><li class="nav-item"><a class="nav-link link text-white display-4" href="#" aria-expanded="false"><span class="mbri-touch mbr-iconfont mbr-iconfont-btn"></span>
+        OY KULLAN &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</a></li><li class="nav-item"><a class="nav-link link text-white display-4" href="#" aria-expanded="false"><span class="mbri-bookmark mbr-iconfont mbr-iconfont-btn"></span>
+        ANKENTLER &nbsp; &nbsp; &nbsp; &nbsp;</a></li><li class="nav-item dropdown open"><a class="nav-link link text-white display-4" href="#" aria-expanded="true"><span class="mbri-globe-2 mbr-iconfont mbr-iconfont-btn"></span>
+        HABERLER &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</a></li></ul>
+    </div>
+</nav>
+</section>
+
+<section class="engine"><a href="#"></a></section><section class="countdown2 cid-qQJJpEXmrb" id="countdown2-q">
+    <div class="container">
+        <h2 class="mbr-section-title pb-3 align-center mbr-fonts-style display-2"></h2>
+        <h3 class="mbr-section-subtitle align-center mbr-fonts-style display-5"></h3>
+    </div>
+    <div class="container pt-5 mt-2">
+        <div class=" countdown-cont align-center p-4">
+            <div class="event-name align-left mbr-white ">
+                <h4 class="mbr-fonts-style display-5">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 24 HAZİRAN SEÇİMLERİ GERİ SAYIM</h4>
+            </div>
+            <div class="countdown align-center py-2" data-due-date="2018/01/01">
+            </div>
+            <div class="daysCountdown" title="Gün"></div>
+            <div class="hoursCountdown" title="Saat"></div>
+            <div class="minutesCountdown" title="Dakika"></div>
+            <div class="secondsCountdown" title="Saniye"></div>
+            <div class="event-date align-left mbr-white">
+                <h5 class="mbr-fonts-style display-7">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; 30.04.2018</h5>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<section class="container cumhur-aday" style = "background-color: white; margin-top: 100px; padding: 0px; margin-bottom: 70px;">
+    <div class="row" style="padding: 0px; margin-left: 20px;">
+        <div class="col-md-3 cumhur-aday">
+            <div class="container img-cumhur">
+              <img src="http://[::1]/dijitalSandik/assets/images/4-01.png" alt="Recep Tayyip ERDOĞAN" class="image img-fluit" style="width:85%; padding: 0px; margin: 0px">
+              <div class="middle">
+			  <form id="form1" name="form1" method="post" action="oy_veritabani.php">
+			  <input type="hidden" name="action" value="erdogan"  />
+			  <button type="submit" class="btn btn-outline-primary" style="border-radius:50%; height:110px; width:110px;">TERCİH</button>
+			  </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-md-3 cumhur-aday">
+        <div class="container img-cumhur">
+          <img src="http://[::1]/dijitalSandik/assets/images/2-01.png" alt="Muharrem İNCE" class="image img-fluit" style="width:85%; padding: 0px;">
+          <div class="middle">
+			<form id="form1" name="form1" method="post" action="oy_veritabani.php">
+			  <input type="hidden" name="action" value="ince"  />
+			  <button type="submit" class="btn btn-outline-primary" style="border-radius:50%; height:110px; width:110px;">TERCİH</button>
+			</form>        
+		</div>
+    </div>
+</div>
+
+<div class="col-md-3 cumhur-aday">
+    <div class="container img-cumhur">
+      <img src="http://[::1]/dijitalSandik/assets/images/3-01.png" alt="Meral AKŞENER" class="image img-fluit" style="width:85%">
+      <div class="middle">
+        <form id="form1" name="form1" method="post" action="oy_veritabani.php">
+			<input type="hidden" name="action" value="aksener"  />
+			<button type="submit" class="btn btn-outline-primary" style="border-radius:50%; height:110px; width:110px;">TERCİH</button>
+		</form>
+    </div>
+</div>
+</div>
+
+<div class="col-md-3 cumhur-aday">
+    <div class="container img-cumhur">
+      <img src="http://[::1]/dijitalSandik/assets/images/1-01.png" alt="Selahattin DEMİRTAŞ" class="image img-fluit" style="width:85%">
+      <div class="middle">
+         <form id="form1" name="form1" method="post" action="oy_veritabani.php">
+			<input type="hidden" name="action" value="demirtas"  />
+			<button type="submit" class="btn btn-outline-primary" style="border-radius:50%; height:110px; width:110px;">TERCİH</button>
+		</form>
+    </div>
+</div>
+</div>
+
+</div>
+
+</section>
+
+
+
+<section class="timeline1 cid-qQJ2vHezCq" id="timeline1-a" style="background-color: #f0f8ff;">
+    <div class="container align-center">
+        <h2 class="mbr-section-title pb-3 mbr-fonts-style display-2">GÜNÜN MANŞETİ ZAMAN ÇİZELGESİ</h2>
+        <h3 class="mbr-section-subtitle pb-5 mbr-fonts-style display-5"></h3>
+
+        <div class="container timelines-container" mbri-timelines="">
+            <div class="row timeline-element reverse separline">      
+               <div class="timeline-date-panel col-xs-12 col-md-6  align-left">         
+                <div class="time-line-date-content">
+                    <p class="mbr-timeline-date mbr-fonts-style display-5">
+                        29 Nisan 2018                           
+                    </p>
+                </div>
+            </div>
+            <span class="iconBackground"></span>
+            <div class="col-xs-12 col-md-6 align-right">
+                <div class="timeline-text-content">
+                    <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                    ismail</h4>
+                    <p class="mbr-timeline-text mbr-fonts-style display-7">
+                        test test test test test test test test test testtest test test test testtest test test test testtest test test test testtest test test test testtest test test test testtest test test test test test test test test testtest test test test testtest test test test testtest test test test testtest test test test testtest test test test test
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row timeline-element  separline">
+            <div class="timeline-date-panel col-xs-12 col-md-6 align-right">
+                <div class="time-line-date-content">
+                    <p class="mbr-timeline-date mbr-fonts-style display-5">
+                    28 Nisan 2018                         </p>
+                </div>
+            </div>
+            <span class="iconBackground"></span>
+            <div class="col-xs-12 col-md-6 align-left ">
+                <div class="timeline-text-content">
+                    <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                    testtt</h4>
+                    <p class="mbr-timeline-text mbr-fonts-style display-7">
+                    test test test test test&nbsp;test test test test testtest test test test testtest test test test testtest test test test testtest test test test testtest test test test test      </p>
+                </div>
+            </div>
+        </div> 
+
+
+        <div class="row timeline-element reverse separline">
+            <div class="timeline-date-panel col-xs-12 col-md-6  align-left">
+                <div class="time-line-date-content">
+                    <p class="mbr-timeline-date mbr-fonts-style display-5">
+                        27 Nisan 2018
+                    </p>
+                </div>
+            </div>
+            <span class="iconBackground"></span>
+            <div class="col-xs-12 col-md-6 align-right">
+                <div class="timeline-text-content">
+                    <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                    ABC</h4>      
+                    <p class="mbr-timeline-text mbr-fonts-style display-7">
+                        asdsaddsdscdsdscdssdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddt.
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row timeline-element  separline">
+            <div class="timeline-date-panel col-xs-12 col-md-6 align-right">
+                <div class="time-line-date-content">
+                    <p class="mbr-timeline-date mbr-fonts-style display-5">
+                    26 Nisan 2018   </p>
+                </div>
+            </div>
+            <span class="iconBackground"></span>
+            <div class="col-xs-12 col-md-6 align-left ">
+                <div class="timeline-text-content">
+                    <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                    SAADET PARTİSİ ÇATI ADAY ÇALIŞMA</h4>
+                    <p class="mbr-timeline-text mbr-fonts-style display-7">
+                    Saadet partisi Abdullah Gül'ün cumhur başkanlığı adaylığı için Chp ve İyi parti genel başkanlarıyla görüştü.</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row timeline-element reverse separline">
+            <div class="timeline-date-panel col-xs-12 col-md-6  align-left">
+                <div class="time-line-date-content">
+                    <p class="mbr-timeline-date mbr-fonts-style display-5">25 Nisan 2018 &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;</p>
+                </div>
+            </div>
+            <span class="iconBackground"></span>
+            <div class="col-xs-12 col-md-6 align-right">
+                <div class="timeline-text-content">
+                    <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                    İYİ PARTİ GRUP KURDU</h4>
+                    <p class="mbr-timeline-text mbr-fonts-style display-7">
+                    Chp'den geçen 15 vekille iyi parti grup kurdu, güneş motel olayını anımsattı.</p>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row timeline-element">
+            <div class="timeline-date-panel col-xs-12 col-md-6 align-right">
+                <div class="time-line-date-content">
+                 <p class="mbr-timeline-date mbr-fonts-style display-5">
+                 24 Nisan 2018 </p>
+             </div>
+         </div>
+         <span class="iconBackground"></span>
+         <div class="col-xs-12 col-md-6 align-left ">
+            <div class="timeline-text-content">
+                <h4 class="mbr-timeline-title pb-3 mbr-fonts-style display-5">
+                İA</h4>
+                <p class="mbr-timeline-text mbr-fonts-style display-7">
+                Lopdjfdjodmcpdsmcds</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+</div>
+</section>
+
+<div class="footer">
+  <p>© Copyright 2018</p>
+</div>
+
+
+        <script src="http://[::1]/dijitalSandik/assets/web/assets/jquery/jquery.min.js"></script>
+        <script src="http://[::1]/dijitalSandik/assets/popper/popper.min.js"></script>
+        <script src="http://[::1]/dijitalSandik/assets/tether/tether.min.js"></script>
+        <script src="http://[::1]/dijitalSandik/assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="http://[::1]/dijitalSandik/assets/countdown/jquery.countdown.min.js"></script>
+        <script src="http://[::1]/dijitalSandik/assets/dropdown/js/script.min.js"></script>
+        <script src="http://[::1]/dijitalSandik/assets/touchswipe/jquery.touch-swipe.min.js"></script>
+        <script src="http://[::1]/dijitalSandik/assets/smoothscroll/smooth-scroll.js"></script>
+        <script src="http://[::1]/dijitalSandik/assets/theme/js/script.js"></script>
+
+
+    </body>
+    </html>
